c29ad1b4e2553557df69b8e4ecf2ba283b29212f
Hannah Malm <hannahemalm616@gmail.com>
2021-03-21 15:18:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,5 @@
-class Dog
-  ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database =>"db/dogs.sqlite")
+class Dog < ActiveRecord::Base
+#   ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database =>"db/dogs.sqlite")
   
-  attr_accessor :name, :breed 
   
-  def initialize(name, breed)
-    @name = name 
-    @breed = breed 
-  end   
-  
-end
+# end
